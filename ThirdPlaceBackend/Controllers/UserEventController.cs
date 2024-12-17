890298ee59a9b7b6b4abc1dcd01402959c23a279
Dmitry Gaimaldinov <dmitrygaimaldinov@gmail.com>
2024-12-17 23:17:41 +0500

    Add project files.

---
+++ ThirdPlaceBackend/Controllers/UserEventController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThirdPlaceBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserEventController : ControllerBase
+    {
+        [HttpGet]
+        public String HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
